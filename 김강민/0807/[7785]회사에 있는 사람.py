07d32772a1f921d92a61b82eae444e0a628dfb47
Dobbymin <79429482+Dobbymin@users.noreply.github.com>
2023-08-07 23:36:04 +0900

    8월 07일 회사에 있는 사람

---
+++ 김강민/0807/[7785]회사에 있는 사람.py
@@ -0,0 +1,17 @@
+import sys
+
+n = int(sys.stdin.readline())
+state = dict()
+
+for _ in range(n):
+    name, attendance = map(str, sys.stdin.readline().split())
+
+    if attendance == "enter":
+        state[name] = attendance
+    else:
+        del state[name]
+
+temp = sorted(state.keys(), reverse=True)
+
+for i in state:
+    print(i)
