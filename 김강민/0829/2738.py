f27ff263f3312a18d6102d32b8044d478f535988
Dobbymin <79429482+Dobbymin@users.noreply.github.com>
2023-08-29 23:25:45 +0900

    8월 29일 행렬 덧셈 (#73)
    * 8월 29일 행렬 덧셈
    
    * 8월 29일 행렬 덧셈

---
+++ 김강민/0829/2738.py
@@ -0,0 +1,21 @@
+import sys
+input = sys.stdin.readline
+
+A = []
+B = []
+C = []
+
+N, M = map(int, input().split())
+
+for i in range(N):
+    i = list(map(int, input().split()))
+    A.append(i)
+
+for i in range(N):
+    i = list(map(int, input().split()))
+    B.append(i)
+
+for i in range(N):
+    for j in range(M):
+        print(A[i][j] + B[i][j], end=' ')
+    print()
