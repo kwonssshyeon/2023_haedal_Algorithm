08c47da99c8a2969acc0ca348a4c080fb2354706
Dobbymin <gmin0701@knu.ac.kr>
2023-09-05 17:12:20 +0900

    9월 5일 듣보잡

--- 김강민/0905/1764.py
+++ 김강민/0905/1764.py
@@ -5,14 +5,19 @@ N, M = map(int, input().split())
 
 noSee = []
 noListen = []
+noSeeListen = []
 
-for _ in range(N):
-    noSee_name = input().strip()
-    noSee.append(noSee_name)
+for i in range(N):
+    name = input().strip()
+    noSee.append(name)
 
-for _ in range(M):
-    noListen_name = input().strip()
-    noListen.append(noListen_name)
+for i in range(M):
+    name = input().strip()    
+    noListen.append(name)
 
-for i in noSee:
+noSeeListen = list(set(noSee) & set(noListen))
+noSeeListen.sort()
+print(len(noSeeListen))
+
+for i in noSeeListen:
     print(i)
