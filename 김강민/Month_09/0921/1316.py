b57e4f12e9d6e3f6641cf529cb66abf3663a9feb
Dobbymin <gmin0701@knu.ac.kr>
2023-09-21 01:05:32 +0900

    [FEAT] 9.20 그룹 단어 체커

---
+++ 김강민/Month_09/0921/1316.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+cnt = N
+
+for i in range(N):
+    word = input().strip()
+    for j in range(0, len(word) - 1):
+        if word[j] == word[j + 1]:
+            pass
+        elif word[j] in word[j + 1: ]:
+            cnt -= 1
+            break
+
+print(cnt)
