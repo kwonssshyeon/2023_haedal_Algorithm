52385e19e7f8e6d36b443c914da275ece5cf37a1
DingX2 <pourding@naver.com>
2023-09-26 01:14:48 +0900

    [230926] 종이 접기 푸는중
    - 부캠준비해야지ㅣㅣㅣ

---
+++ 이수현/230925/[1802] 종이 접기.py
@@ -0,0 +1,20 @@
+import sys
+
+input = sys.stdin.readline
+
+
+def origami(test_case):
+    length = len(test_case) // 2
+    print(length)
+    for i in range(length + 1):  # 접는 회수
+        print(test_case[i * 2], i * 2)
+        return "YES"
+    return "NO"
+
+
+N = int(input())
+test = []
+for _ in range(N):
+    test.append(input().rstrip())
+for test_case in test:
+    print(origami(test_case))
