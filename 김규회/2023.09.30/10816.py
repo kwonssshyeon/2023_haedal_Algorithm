5710c457badece84439016607ec6f0bee9083cfe
Quvid <48755156+KimKyuHoi@users.noreply.github.com>
2023-09-30 18:27:23 +0900

    '

---
+++ 김규회/2023.09.30/10816.py
@@ -0,0 +1,24 @@
+# 10816.py
+# https://www.acmicpc.net/problem/10816
+
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+a = sorted(list(map(int,input().split())))
+m = map(int,input().split())
+b = list(map(int,input().split()))
+
+cnt = {}
+
+for i in a:
+    if i in cnt:
+        cnt[i] += 1
+    else:
+        cnt[i] = 1
+        
+for i in b:
+    if i in cnt:
+        print(cnt[i], end = ' ')
+    else:
+        print(0, end = ' ' )
