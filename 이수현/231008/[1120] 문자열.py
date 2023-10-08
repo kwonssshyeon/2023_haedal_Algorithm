4b46b5614196dcfc1e56cf0419c1a03d18b55692
DingX2 <pourding@naver.com>
2023-10-09 01:55:10 +0900

    231008 피보나치 함수, 문자열
    - 11279 최대힙 풀어야함

---
+++ 이수현/231008/[1120] 문자열.py
@@ -0,0 +1,15 @@
+import sys
+
+input = sys.stdin.readline
+
+a, b = input().split()
+
+answer = []
+for i in range(len(b) - len(a) + 1):
+    count = 0
+    for j in range(len(a)):
+        if a[j] != b[i + j]:
+            count += 1
+    answer.append(count)
+
+print(min(answer))
