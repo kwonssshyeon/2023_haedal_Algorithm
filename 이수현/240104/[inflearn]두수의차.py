3e788a269c8ff4cce70aa40554be84086131c0af
DingX2 <pourding@naver.com>
2024-01-04 23:20:03 +0900

    [Feat]: 240104 강의듣고 풀이 사탕종류

---
+++ 이수현/240104/[inflearn]두수의차.py
@@ -0,0 +1,16 @@
+import sys
+
+input = sys.stdin.readline()
+
+
+def solution(nums):
+    answer = []
+
+    return answer
+
+
+print(solution([3, 8, 1, 5, 12]))
+print(solution([2, 1, 3, 5, 4]))
+print(solution([5, 10, 15, 20, 25, 11]))
+print(solution([2, 4, 3, 1, 5, 7, 8, 12, 13, 15, 23]))
+print(solution([100, 200, 300, 400, 120, 130, 135, 132, 121]))
