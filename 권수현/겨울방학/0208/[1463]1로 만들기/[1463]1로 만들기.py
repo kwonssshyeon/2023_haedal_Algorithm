cde743fd2536b3a1812e9d3db0881cb6604a0830
kwonssshyeon <104684033+kwonssshyeon@users.noreply.github.com>
2024-02-08 19:32:48 +0900

    0208 kwon (#183)
    * 0206_kwon
    
    * 0208_kwon

---
+++ 권수현/겨울방학/0208/[1463]1로 만들기/[1463]1로 만들기.py
@@ -0,0 +1,26 @@
+# 17:45 ~
+
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+
+dp = [0]*(n+1)
+
+if n==1:
+    print(0)
+    sys.exit(0)
+
+
+for i in range(2,n+1):
+    if i%6==0:
+        dp[i] = min(dp[i-1],dp[i//3],dp[i//2])+1
+    elif i%3==0:
+        dp[i] = min(dp[i-1],dp[i//3])+1
+    elif i%2==0:
+        dp[i] = min(dp[i-1],dp[i//2])+1
+    else:
+        dp[i] = dp[i-1]+1
+
+
+print(dp[n])
